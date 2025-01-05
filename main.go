87548a5743d31a787a6bb1cc1903261a19d7eca2
poteto <naonyan1135@gmail.com>
2025-01-05 23:40:40 +0900

    すげえ

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -36,10 +38,7 @@ func main() {
 	r.GET("/auth/:provider", func(c *gin.Context) {
 		provider := c.Param("provider")
 		c.Request = contextWithProviderName(c, provider)
-		fmt.Println(provider)
 		gothic.BeginAuthHandler(c.Writer, c.Request)
-		// fmt.Println(provider)
-		// c.String(201, provider)
 	})
 
 	r.GET("/auth/:provider/callback", func(c *gin.Context) {
@@ -53,11 +52,85 @@ func main() {
 			return
 		}
 		log.Printf("ユーザー情報: %#v", user)
+
+		cookie, err := c.Cookie("user")
+
+		if err != nil {
+			cookie = "NotSet"
+			c.SetCookie("user", user.UserID, 3600, "/", "localhost", false, true)
+		} else {
+			c.SetCookie("user", user.UserID, 3600, "/", "localhost", false, true)
+		}
+
+		fmt.Printf("Cookie value: %s \n", cookie)
+		fmt.Println(c.Cookie("gin_cookie"))
+
+		// c.JSON(200, gin.H{
+		// 	"username":  user.NickName,
+		// 	"avatarUrl": user.AvatarURL,
+		// })
+
+		c.Redirect(http.StatusTemporaryRedirect, "/whoamI")
+	})
+
+	r.GET("/whoamI", func(c *gin.Context) {
+		cookie, err := c.Cookie("user")
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(200, gin.H{"user": nil})
+		}
+
+		url := "https://api.github.com/user/" + cookie
+		fmt.Println(url)
+
+		res, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+			c.String(200, err.Error())
+			return
+		}
+		defer res.Body.Close()
+
+		// レスポンスボディを読み取る
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatalf("Error reading response body: %v", err)
+		}
+
+		var user Response
+		if res.StatusCode != http.StatusOK {
+			c.String(200, "aaa")
+			return
+		}
+
+		err2 := json.Unmarshal(body, &user)
+		if err2 != nil {
+			fmt.Println("Error unmarshaling JSON: ", err)
+			c.String(200, err2.Error())
+			return
+		}
+
+		fmt.Println(user)
+		// 構造体のデータを利用
+		fmt.Printf("Username: %s\n", user.UserName)
+		fmt.Printf("ID: %d\n", user.ID)
+		fmt.Printf("Public_repos: %d\n", user.Public_Repos)
+		fmt.Printf("Followers: %d\n", user.Followers)
+
 		c.JSON(200, gin.H{
-			"username":  user.NickName,
-			"avatarUrl": user.AvatarURL,
+			"cookie": cookie,
+			"user":   user,
 		})
+
 	})
 
 	r.Run(":4000")
+}
+
+// JSONレスポンスに対応する構造体を定義
+type Response struct {
+	UserName     string `json:"login"`
+	ID           int    `json:"id"`
+	Public_Repos int    `json:"public_repos"`
+	Followers    int    `json:"followers"`
 }
